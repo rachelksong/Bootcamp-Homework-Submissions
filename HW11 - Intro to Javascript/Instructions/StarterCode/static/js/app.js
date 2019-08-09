5a915aeb899bb8ac2a7d516c014a0306a1eee94f
Rachel Song <rachelksong@gmail.com>
2019-08-09 10:32:13 -0400

    HW11 commit

--- HW11 - Intro to Javascript/Instructions/StarterCode/static/js/app.js
+++ HW11 - Intro to Javascript/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,34 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//Grab tbody information
+tbody = d3.select("tbody")
+
+function displayData(data){
+    tbody.text("")
+    data.forEach(function(sighting){
+    new_tr = tbody.append("tr")
+    Object.entries(sighting).forEach(function([key, value]){
+        new_td = new_tr.append("td").text(value)
+    })
+})}
+
+displayData(tableData)
+
+//Selecting web user's input; filter button
+var date_input = d3.select("#datetime")
+var button = d3.select("filter-btn")
+
+//Filter the data by date of user's input
+function clickSelect(){
+    //Don't refresh the page!
+    d3.event.preventDefault();
+    //Print input value
+    console.log(date_input.property("value"));
+    //Create new table based on filtered data
+    var new_table = tableData.filter(incident => incident.datetime===date_input.property("value")))
+    //Display the new table
+    showtable(new_table);
+}
+
+date_input.on("change", clickSelect)
